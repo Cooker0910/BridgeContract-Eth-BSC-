2e610df9edbc6ceb7685de731fa63044f40e91fe
cooker0910 <cooker98910@gmail.com>
2022-06-22 07:34:42 -0700

    update

--- contracts/IToken.sol
+++ contracts/IToken.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 
 interface IToken {
-function mint(address to, uint amount) external;
-function burn(address owner, uint amount) external;
+    function mint(address to, uint256 amount) external;
+
+    function burn(address owner, uint256 amount) external;
 }
--- contracts/TokenEth.sol
+++ contracts/TokenEth.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.4;
 import "./TokenBase.sol";
 
 contract TokenEth is TokenBase {
-    constructor() TokenBase("ETH Token", "ETK") {}
+  constructor() TokenBase("ETH Token", "ETK") {}
 }
