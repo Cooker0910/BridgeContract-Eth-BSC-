4ee7dd21c23f1e62fd21f825646e5325e9195e5b
cooker0910 <cooker98910@gmail.com>
2022-06-23 00:46:10 -0700

    update

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const mnemonic = 'enemy bulb hotel leave lens quote decrease thunder silent film father man';
+const mnemonic = '';
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
