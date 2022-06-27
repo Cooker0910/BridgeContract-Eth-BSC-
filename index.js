a22f3981795b68bd03b640d914b1a83ba82b536f
cooker0910 <cooker98910@gmail.com>
2022-06-27 00:11:52 -0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Web3 = require('web3');
-const BridgeEth = require('../build/contracts/BridgeEth.json');
-const BridgeBsc = require('../build/contracts/BridgeBsc.json');
-const web3Eth = new Web3('url to eth node (websocket)');
+require('dotenv').config()
+
+const BridgeEth = require('./build/contracts/BridgeEth.json');
+const BridgeBsc = require('./build/contracts/BridgeBsc.json');
+const web3Eth = new Web3('wss://rinkeby.infura.io/ws/v3/0e42c582d71b4ba5a8750f688fce07da');
 const web3Bsc = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
-const adminPrivKey = '';
+const adminPrivKey = process.env.privateKey;
 const { address: admin } = web3Bsc.eth.accounts.wallet.add(adminPrivKey);
 const bridgeEth = new web3Eth.eth.Contract(
   BridgeEth.abi,
@@ -13,10 +15,12 @@ const bridgeBsc = new web3Bsc.eth.Contract(
   BridgeBsc.abi,
   BridgeBsc.networks['97'].address
 );
+
 bridgeEth.events.Transfer(
   { fromBlock: 0, step: 0 }
 )
   .on('data', async event => {
+    console.log('-------------------', event)
     const { from, to, amount, date, nonce, signature } = event.returnValues;
     const tx = bridgeBsc.methods.mint(from, to, amount, nonce, signature);
     const [gasPrice, gasCost] = await Promise.all([
@@ -32,6 +36,6 @@ bridgeEth.events.Transfer(
       gasPrice
     };
     const receipt = await web3Bsc.eth.sendTransaction(txData);
-    console.log(`Transaction hash: ${ receipt.transactionHash }`);
-    console.log(`Processed transfer: - from ${ from } - to ${ to } - amount ${ amount } tokens - date ${ date } - nonce ${ nonce }`);
+    console.log(`Transaction hash: ${receipt.transactionHash}`);
+    console.log(`Processed transfer: - from ${from} - to ${to} - amount ${amount} tokens - date ${date} - nonce ${nonce}`);
   });
